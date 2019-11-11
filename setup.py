9170b85909d015ad474f8f6347fae3bf5863bba4
Kevin Duff <kduff@enthought.com>
2019-11-12 00:13:57 +0900

    Update setup.py to prepare for first release

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="typen-k2bd",
+    name="typen",
     version="0.0.1",
     author="Kevin Duff",
     author_email="kduff@enthought.com",
-    description="Enforce function type hints with Traits",
+    description="Strong type hints with Traits",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/k2bd/typen",
@@ -17,6 +17,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
     ],
     python_requires='>=3.6',
 )
