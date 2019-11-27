13b0241a1898907bf2d8b5c85393f52995e8e9af
Kevin Duff <kduff@enthought.com>
2019-11-27 21:19:36 +0900

    Remove class definition from decroation

--- typen/_decorators.py
+++ typen/_decorators.py
@@ -7,7 +7,7 @@ def enforce_type_hints(func):
     """
     Enforce type hints on the parameters and return types decorated function.
     """
-    return configure_enforce_type_hints(require_args=False, require_return=False)(func)
+    return EnforceTypeHints(func, require_args=False, require_return=False)
 
 
 def strict_type_hints(func):
@@ -17,7 +17,7 @@ def strict_type_hints(func):
     Also require type hints to be provided for all parmeters and the return
     value.
     """
-    return configure_enforce_type_hints(require_args=True, require_return=True)(func)
+    return EnforceTypeHints(func, require_args=True, require_return=True)
 
 
 def strict_parameter_hints(func):
@@ -29,7 +29,7 @@ def strict_parameter_hints(func):
     Note: This decorator does NOT stack with ``strict_return_hints``. Please
     use ``strict_type_hints`` to enforce both parameter and return value hints.
     """
-    return configure_enforce_type_hints(require_args=True, require_return=False)(func)
+    return EnforceTypeHints(func, require_args=True, require_return=False)
 
 
 def strict_return_hint(func):
@@ -41,60 +41,60 @@ def strict_return_hint(func):
     Note: This decorator does NOT stack with ``strict_parameter_hints``. Please
     use ``strict_type_hints`` to enforce both parameter and return value hints.
     """
-    return configure_enforce_type_hints(require_args=False, require_return=True)(func)
+    return EnforceTypeHints(func, require_args=False, require_return=True)
 
 
-def configure_enforce_type_hints(require_args=False, require_return=False):
-    class EnforceTypeHints:
-        def __init__(self, func):
-            self.func = func
-            self.enforcer = None
+class EnforceTypeHints:
+    def __init__(self, func, require_args, require_return):
+        self.func = func
+        self.enforcer = None
+        self.require_args = require_args
+        self.require_return = require_return
 
-        def __call__(self, *args, **kwargs):
-            if self.enforcer is None:
-                self.decorate()
+    def __call__(self, *args, **kwargs):
+        if self.enforcer is None:
+            self.decorate()
 
-            return self.decorated_func(*args, **kwargs)
+        return self.decorated_func(*args, **kwargs)
 
-        def __set_name__(self, owner, name):
-            # This is called on class creation so we can distinguish methods
-            # from non-methods
+    def __set_name__(self, owner, name):
+        # This is called on class creation so we can distinguish methods
+        # from non-methods
 
-            ignore_self = True
+        ignore_self = True
 
-            # Get the actual function if this is a static or class method
-            desc = None
-            if isinstance(self.func, staticmethod):
-                # Static methods don't take a `self` parameter
-                desc = staticmethod
-                ignore_self = False
-            elif isinstance(self.func, classmethod):
-                desc = classmethod
+        # Get the actual function if this is a static or class method
+        desc = None
+        if isinstance(self.func, staticmethod):
+            # Static methods don't take a `self` parameter
+            desc = staticmethod
+            ignore_self = False
+        elif isinstance(self.func, classmethod):
+            desc = classmethod
 
-            if desc:
-                self.func = self.func.__func__
+        if desc:
+            self.func = self.func.__func__
 
-            self.decorate(ignore_self=ignore_self)
+        self.decorate(ignore_self=ignore_self)
 
-            if desc:
-                self.decorated_func = desc(self.decorated_func)
-            setattr(owner, name, self.decorated_func)
+        if desc:
+            self.decorated_func = desc(self.decorated_func)
+        setattr(owner, name, self.decorated_func)
 
-        def decorate(self, ignore_self=False):
-            self.enforcer = Enforcer(
-                self.func,
-                require_args=require_args,
-                require_return=require_return,
-                ignore_self=ignore_self,
-            )
+    def decorate(self, ignore_self=False):
+        self.enforcer = Enforcer(
+            self.func,
+            require_args=self.require_args,
+            require_return=self.require_return,
+            ignore_self=ignore_self,
+        )
 
-            @wraps(self.func)
-            def new_func(*args, **kwargs):
-                self.enforcer.verify_args(args, kwargs)
-                result = self.func(*args, **kwargs)
-                self.enforcer.verify_result(result)
-                return result
+        @wraps(self.func)
+        def new_func(*args, **kwargs):
+            self.enforcer.verify_args(args, kwargs)
+            result = self.func(*args, **kwargs)
+            self.enforcer.verify_result(result)
+            return result
 
-            self.decorated_func = new_func
+        self.decorated_func = new_func
 
-    return EnforceTypeHints
