eefeaf977da82166a06538c16bf3f00c814399da
Kevin Duff <kduff@enthought.com>
2019-11-28 23:21:35 +0900

    Attempt at 3.7+ support

--- typen/_typing.py
+++ typen/_typing.py
@@ -24,14 +24,13 @@ def typing_to_trait(arg_type):
     origin = arg_type.__origin__ or arg_type
 
     if origin in [typing.List, list]:
-        if arg_type.__args__ is not None:
+        if arg_type.__args__ is not None and arg_type.__args__ is not typing.Any:
             contained = arg_type.__args__[0]
-            print(contained)
             return ValidatedList(typing_to_trait(contained))
         else:
             return traits_api.List()
     elif origin in [typing.Tuple, tuple]:
-        if arg_type.__args__ is not None:
+        if arg_type.__args__ is not None and arg_type.__args__ is not typing.Any:
             contained = [typing_to_trait(arg) for arg in arg_type.__args__]
             return traits_api.Tuple(*contained)
         else:
