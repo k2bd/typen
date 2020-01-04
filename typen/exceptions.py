344c4bf55f4c38bf85c006a6656f89033c6dbb8d
Kevin Duff <kduff@enthought.com>
2020-01-04 14:08:01 +0000

    Tidy enforcer & add docstrings

--- typen/exceptions.py
+++ typen/exceptions.py
@@ -1,16 +1,28 @@
 class UnspecifiedParameterTypeError(Exception):
+    """
+    Parameter type hint required but not specified
+    """
     pass
 
 
 class UnspecifiedReturnTypeError(Exception):
+    """
+    Return type hint required but not specified
+    """
     pass
 
 
 class ParameterTypeError(Exception):
+    """
+    Passed parameter is invalid
+    """
     pass
 
 
 class ReturnTypeError(Exception):
+    """
+    Return type is invalid
+    """
     pass
 
 
